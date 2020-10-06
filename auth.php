7078fb247103a5325a15c03c18eb6014dc6b3944
l1fsec <54239712+l1fsec@users.noreply.github.com>
2020-10-06 19:54:10 +0200

    Přidávání úprav
    Hell ye

--- auth.php
+++ auth.php
@@ -6,7 +6,6 @@ $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
-
 // Vyzkoušej připojení
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
@@ -25,7 +24,7 @@ if ($stmt = $con->prepare('SELECT id, psw FROM accounts WHERE name = ?')) {
 	if ($stmt->num_rows > 0) {
 	$stmt->bind_result($id, $psw);
 	$stmt->fetch();
-	
+
 	if ($_POST['psw'] === $psw) { //Uspěšně připojeno
 		session_regenerate_id();
 		$_SESSION['loggedin'] = TRUE;
--- logged/profile.php
+++ logged/profile.php
@@ -6,10 +6,12 @@ if (!isset($_SESSION['loggedin'])) {
 	exit;
 }
 //Prihlasovaci udaje pro DB
+//------------------------------
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
+//-----------------------------
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
